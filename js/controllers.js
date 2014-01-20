c2192784b904b9d2e7a2e79866a36f7c3f61aa6d
Rob Larsen <rob@htmlcssjavascript.com>
2014-01-20 15:49:15 -0500

    updates for the record comic book sales thread

--- js/controllers.js
+++ js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('comicsApp.controllers', ['comicFilters']).
+angular.module('comicsApp.controllers', ['comicFilters','comicsFactories']).
   controller('comicsCtrl', ["$scope", "$http",
     function( $scope , $http)  {
       $http({"method" : "GET", "url" : "/data/books.json"}).success(
@@ -63,7 +63,7 @@ angular.module('comicsApp.controllers', ['comicFilters']).
         for ( var i = 0; i <len; i++){
           delete items[i]["$$hashKey"];
         }
-        $http.post('data/index.php', JSON.stringify(items)).success(function(data){console.log("200");
+        $http.post('data/index.php', angular.toJson(items)).success(function(data){console.log("200");
         }); 
       } 
       $scope.addSale = function(sale) {
@@ -97,4 +97,75 @@ angular.module('comicsApp.controllers', ['comicFilters']).
       	
       } 
   }
-])
+]).controller('recordCtrl', ["$scope",'dataService',
+    function( $scope, dataService )  {
+		$scope.items = dataService;
+		$scope.sort = "-price";
+		$scope.sorter = function(sort){
+       
+       if ($scope.sort.indexOf(sort) >-1 && $scope.sort.charAt(0) === "-" ) {
+          
+            $scope.sort = sort.slice(1);
+          
+      	}
+        if ($scope.sort.indexOf(sort) >-1 && $scope.sort.indexOf("-") == -1 ) {
+          
+            $scope.sort = "-"+sort;
+          
+        } else {
+         $scope.sort = sort;
+        }
+      	
+      	
+      } 
+	}
+	
+]).controller('chartCtrl', ["$scope",'dataService',
+    function( $scope, dataService )  {
+    $scope.items = dataService;
+    $scope.tooltip = {
+      price:0
+    }
+    $scope.updateTooltip = function(it) {
+      $scope.tooltip = {
+        price:it.price || 0,
+        venue:it.venue,
+        date:it.date,
+		title:it.title,
+		issue:it.issue,
+		pedigree:it.pedigree,
+		collection:it.collection,
+		provenance:it.provenance,
+		grade_src: it.grade_src,
+		grade : it.grade
+      } 
+    }
+    $scope.colorPicker= function( venue ){
+
+      switch (venue) {
+        case "Heritage":
+          return $scope.colors[0];
+        case  "Comic Connect":
+          return $scope.colors[1];
+        case "Comiclink":
+          return $scope.colors[2];
+        case  "Pedigree":
+          return $scope.colors[3];
+        case "Metropolis":
+          return $scope.colors[4];
+        case  "JP The Mint":
+          return $scope.colors[5];
+        case "Mastronet":
+          return $scope.colors[6];
+        case  "PGCMint":
+          return $scope.colors[7];
+        default:
+          return $scope.colors[8];
+       }
+     
+    }
+    $scope.colors = ["#ECD078","#D95B43","#C02942","#542437","#53777A","#69D2E7","#FA6900", "#FE4365","#666666"];
+    
+  }
+  
+]);
